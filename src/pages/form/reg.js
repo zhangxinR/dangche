b9951d55c716c4127812c87dc3213ee3bedb4c76
zhangxin <zhangxin@polysoft.com.cn>
2018-09-14 18:28:01 +0800

    完成注册页面布局

--- src/pages/form/reg.js
+++ src/pages/form/reg.js
@@ -10,13 +10,18 @@ import {
     Select,
     Switch,
     DatePicker,
-    TimePicker
+    TimePicker,
+    InputNumber
 } from 'antd';
+import moment from 'moment';
 
 const FormItem=Form.Item;
 class Regs extends Component{
     render(){
         const { getFieldDecorator } =this.props.form;
+        const RadioGroup=Radio.Group;
+        const Option=Select.Option;
+        const { TextArea } = Input;
         const formItemLayout = {
             labelCol:{
                 xs:24,
@@ -59,6 +64,91 @@ class Regs extends Component{
                                     ]
                                 })(
                                     <Input placeholder='请输入密码' />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='性别' {...formItemLayout}>
+                            {
+                                getFieldDecorator('sex',{
+                                    initialValue:1
+                                })(
+                                    <RadioGroup>
+                                        <Radio value={1}>男</Radio>
+                                        <Radio value={0}>女</Radio>
+                                    </RadioGroup>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='年龄' {...formItemLayout}>
+                            {
+                                getFieldDecorator('age',{
+                                    initialValue:18
+                                })(
+                                   <InputNumber min={1} max={10} ></InputNumber>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='当前状态' {...formItemLayout}>
+                            {
+                                getFieldDecorator('state',{
+                                    initialValue:'2'
+                                })(
+                                   <Select>
+                                       <Option value='1'>咸鱼一条</Option>
+                                       <Option value='2'>风华浪子</Option>
+                                       <Option value='3'>北大才子</Option>
+                                       <Option value='4'>百度FE</Option>
+                                       <Option value='5'>创业者</Option>
+                                   </Select>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='爱好' {...formItemLayout}>
+                            {
+                                getFieldDecorator('interesting',{
+                                    initialValue:'2'
+                                })(
+                                   <Select mode="multiple">
+                                       <Option value='1'>游泳</Option>
+                                       <Option value='2'>打篮球</Option>
+                                       <Option value='3'>踢足球</Option>
+                                       <Option value='4'>跑步</Option>
+                                       <Option value='5'>爬山</Option>
+                                       <Option value='6'>骑行</Option>
+                                       <Option value='7'>桌球</Option>
+                                       <Option value='8'>麦霸</Option>
+                                   </Select>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='是否已婚' {...formItemLayout}>
+                            {
+                                getFieldDecorator('isMarry',{
+                                    valuePropName:'checked',
+                                    initialValue:true
+                                })(
+                                   <Switch />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='生日' {...formItemLayout}>
+                            {
+                                getFieldDecorator('birthday',{
+                                    initialValue:moment('2018-9-14 12:00:55')
+                                })(
+                                   <DatePicker 
+                                        showTime
+                                        format="YYYY-MM-DD HH:mm:ss"
+                                   />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label='联系地址' {...formItemLayout}>
+                            {
+                                getFieldDecorator('address',{
+                                    initialValue:'北京潘家园美景东方小区'
+                                })(
+                                   <TextArea row={4} />
                                 )
                             }
                         </FormItem>
